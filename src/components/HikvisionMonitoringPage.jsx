70d8966b62e1acf0059d3c3de18b70a9bb8964f5
a2249341476 <145363367+a2249341476@users.noreply.github.com>
2024-09-25 15:55:20 +0800

    Update HikvisionMonitoringPage.jsx

--- src/components/HikvisionMonitoringPage.jsx
+++ src/components/HikvisionMonitoringPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const HikvisionMonitoringPage = () => {
   useEffect(() => {
-    const rtsp = 'rtsp://101.69.216.133:8604/EUrl/PKGWPGA?params=your_rtsp_params';
+    const rtsp = 'rtsp://101.69.216.133:8604/EUrl/PKGWPGA?params=eyJwcm90b2NvbCI6InJ0c3AiLCJleHBhbmQiOiJzdHJlYW1mb3JtPXJ0cCIsInVzZXJJZCI6IuWMluWtpuWTgeivleWJguS7k+W6kzQiLCJ0IjoxLCJhIjoiZWQ2MDViYTQ3N2VmNDI2NmFhYjgxNTM4ZTA3MTVjMzl8MXwwfDF8b3Blbl9hcGkifQ==';
     const player = new JSMpeg.Player(`ws://localhost:9999/rtsp?url=${btoa(rtsp)}`, {
       canvas: document.getElementById('hikvision-canvas'),
     });
